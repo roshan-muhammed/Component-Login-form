efdb7e89c0dd7cf61f1d995b3342fff62c39caa1
roshan-muhammed <muhammedroshan514@gmail.com>
2022-12-06 14:28:12 +0530

    Visually completed
    Added all the validation and styles.
    Next need to clean up the modules

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 'use strict'
 import z from './validation.js'
 const passwordStrength = document.querySelector('#password-strength')
-const formElements = document.querySelectorAll('.form-element')
+const passwordHintGroup = document.querySelector('.password-hint-group')
+const formElements = document.querySelectorAll('.validate')
 const errorBox = document.querySelectorAll('.error-msg')
 const navToggle = document.querySelector('.nav-toggle')
 const navMenu = document.querySelector('#nav-menu')
 const header = document.querySelector('header')
 const formButton = document.querySelector('#form-button')
+const validateIcon = document.querySelectorAll('.validate-icon-e')
 let validated = false
-console.log(formElements)
+let fillable = []
 accessibility()
 
 window.addEventListener('resize', () => {
@@ -33,32 +35,67 @@ function accessibility() {
 formButton.addEventListener('click', prevent)
 
 for (let i = 0; i < formElements.length; i++) {
-    formElements[i].addEventListener("input", () => {
-        if (formElements[i].name == 'name') {
-            console.log(formElements[i].value)
-            showError(z.validateName(formElements[i].value, 3, 16), i)
-            console.log(z.validateName(formElements[i].value))
-        } else if (formElements[i].name == 'password') {
-            let error = z.validatePassword(formElements[i].value)
-            showError(error, i)
-            passwordStrength.innerHTML = error.strength
-            passwordStrength.className = error.strength
-        }
-        if (validated) {
-            formButton.removeEventListener('click', prevent)
-        }
-    })
+    formElements[i].addEventListener("input", () => { validateItems(i) })
 }
 
-function prevent(event){
+function prevent(event) {
     event.preventDefault()
+    if (!validated) {
+        for (let i in formElements) {
+            validateItems(i) && validated++
+        }
+        if (validated = formElements.length) formButton.click()
+    } else {
+        formButton.click()
+    }
 }
-
 
 function showError(error, i) {
     if (!error.status) {
         errorBox[i].innerHTML = error.message
+        validateIcon[i].setAttribute('src', 'images/delete.png')
     } else {
         errorBox[i].innerHTML = ''
+        validateIcon[i].setAttribute('src', 'images/check.png')
     }
+    return error.status
 }
+
+function validateItems(i) {
+
+    
+    if (validateIcon[i].classList != undefined) {
+        validateIcon[i].classList.remove('hidden')
+    }
+
+    if (formElements[i].name == 'name') {
+        fillable[i] = showError(z.validateName(formElements[i].value, 3, 16), i)
+    }
+
+    else if (formElements[i].name == 'password') {
+        passwordHintGroup.classList.remove('hidden')
+        let error = z.validatePassword(formElements[i].value)
+
+        fillable[i] = showError(error, i)
+        passwordStrength.innerHTML = error.strength
+        passwordStrength.className = error.strength
+    }
+    else if (formElements[i].name == 'confirm') {
+        fillable[i] = showError(z.confirmPassword(formElements[i - 1].value, formElements[i].value), i)
+    }
+
+    else if (formElements[i].name == 'phone') {
+        fillable[i] = showError(z.validatePhone(formElements[i].value), i)
+    }
+
+    else if (formElements[i].name == 'date') {
+        fillable[i] = showError(z.validateDate(formElements[i].value), i)
+    }
+
+    console.log([...fillable])
+    validated = z.checkFillable(fillable)
+    if(validated) formButton.removeEventListener('click',prevent)
+    return validated
+}
+
+
--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,4 @@
 
-
 export function validateName(value, min, max, name = 'name') {
     let status = true
     let message = `valid ${name}`
@@ -20,22 +19,27 @@ export function validateName(value, min, max, name = 'name') {
     return { status: status, message: message }
 }
 
-export function validatePassword(value, min = 6, max = 16, digits = true, special = true) {
+export function validatePassword(value, min = 6, max = 16, digits = true, special = false) {
     let message
     let status = true
     let strengthScore = 0
+    let minStatus = true
     let len = max - value.length
     let format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     const strength = ['invalid', 'weak', 'good', 'strong', 'very strong', 'excellent']
     if (value === '') {
         message = `Please enter your password`
         status = false
+        minStatus = false
     } else if (value.length < min) {
         message = `Should be at least ${min} characters`
         status = false
+        minStatus = false
     } else if (value.length > max) {
-        message = `Should be at most ${max} characters`
+        message = `Password can't be over ${max} characters`
         status = false
+        minStatus = false
+        strengthScore = 0
     } else if (!/[A-Z]/.test(value)) {
         status = false
         message = `Should contain 1 upper case letter`
@@ -46,12 +50,74 @@ export function validatePassword(value, min = 6, max = 16, digits = true, specia
         message = `Should contain at least 1 special character`
         status = false
     }
-    if (len > 3 && len < 7) strengthScore++
-    else if (len > 7) strengthScore += 2
-    if (/[A-Z]/.test(value)) strengthScore++
-    if (/[0-9]/.test(value)) strengthScore++
-    if (format.test(value)) strengthScore++
+    if (len > 0) {
+        if (len < 3) strengthScore += 2
+        else if (len < 7) strengthScore++
+
+    }
+    if (minStatus && /[A-Z]/.test(value)) strengthScore++
+
+    if (minStatus && /[0-9]/.test(value)) strengthScore++
+
+
+    if (minStatus && format.test(value)) strengthScore++
     return { status: status, message: message, strength: strength[strengthScore] }
 }
 
-export default { validateName, validatePassword }
+export function confirmPassword(password, confirm) {
+    let status = true
+    let message = ''
+    if (password != confirm || password == '') {
+        status = false
+        message = `Passwords do not match`
+    }
+    return { status: status, message: message }
+}
+
+export function validatePhone(value) {
+    let message = ''
+
+    let status = true
+    let format = /^\+{0,2}([\-\. ])?(\(?\d{0,3}\))?([\-\. ])?\(?\d{0,3}\)?([\-\. ])?\d{3}([\-\. ])?\d{4}/
+    if (value.length < 7) {
+        status = false
+        message = 'Phone number should be at least 7 digits long'
+    }
+    if (!format.test(value)) {
+        status = false
+        message = `Please enter a valid phone number`
+    }
+    return { status: status, message: message }
+}
+
+
+export function validateDate(value) {
+    let message = ''
+    let status = true
+    if (value=='') {
+        status = false
+        message = `Please choose a date`
+    }
+    console.log(value)
+    return { status: status, message: message }
+}
+
+export function validateEmail(value) {
+    let format = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+    let message = ''
+    let status = true
+    if (!format.test(value)) {
+        status = false
+        message = `Please enter a valid email`
+    }
+    return { status: status, message: message }
+}
+
+export function checkFillable(fillable) {
+    for(let state of fillable) {
+        if (state === false) return false
+    }
+    return true
+}
+
+export default { validateName, validatePassword, validatePhone, validateDate, confirmPassword,checkFillable }
